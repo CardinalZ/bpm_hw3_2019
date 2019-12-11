3239b7da71b160b1cbc71f2f06890ef165dfa32e
flamingo <zsjperiod@foxmail.com>
2019-12-11 23:14:27 +0800

    登录页样式修改

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Layout v-if="!isLogin" style="height: 100%">
+    <Layout v-if="!isLogin" style="height: 100vh">
       <Login/>
     </Layout>
 
@@ -13,122 +13,122 @@
       </Header>
       <Layout :style="{minHeight: '100vh'}">
         <Sider :style="{padding:'10px 0'}" class="white-bg">
-            <Menu ref="menu" :theme="'light'" active-name="'visit'" width="auto" @on-select="onSelected">
-              <div v-if="role==='merchant'">
-                <MenuItem name="CommodityList">
-                  <Icon type="md-document" />
-                  <span>商品信息查看与上传</span>
-                </MenuItem>
-                <MenuItem name="OrderList">
-                  <Icon type="md-document" />
-                  <span>订单信息审核</span>
-                </MenuItem>
-              </div>
-              <div v-if="role==='platformManager'">
-                <MenuItem name="CommodityReviewList">
-                  <Icon type="md-pulse" />
-                  <span>商品信息审核</span>
-                </MenuItem>
-                <MenuItem name="QueryAndSort">
-                  <Icon type="md-pulse" />
-                  <span>信息查询及分类</span>
-                </MenuItem>
-              </div>
-            </Menu>
+          <Menu ref="menu" :theme="'light'" active-name="'visit'" width="auto" @on-select="onSelected">
+            <div v-if="role==='merchant'">
+              <MenuItem name="CommodityList">
+                <Icon type="md-document"/>
+                <span>商品信息查看与上传</span>
+              </MenuItem>
+              <MenuItem name="OrderList">
+                <Icon type="md-document"/>
+                <span>订单信息审核</span>
+              </MenuItem>
+            </div>
+            <div v-if="role==='platformManager'">
+              <MenuItem name="CommodityReviewList">
+                <Icon type="md-pulse"/>
+                <span>商品信息审核</span>
+              </MenuItem>
+              <MenuItem name="QueryAndSort">
+                <Icon type="md-pulse"/>
+                <span>信息查询及分类</span>
+              </MenuItem>
+            </div>
+          </Menu>
         </Sider>
-          <Content :style="{padding: '10px 16px 16px'}">
-              <router-view/>
-          </Content>
+        <Content :style="{padding: '10px 16px 16px'}">
+          <router-view/>
+        </Content>
       </Layout>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import Login from './components/Login'
-import APIUtil from './services/APIUtil'
+  import {mapGetters} from 'vuex'
+  import Login from './components/Login'
+  import APIUtil from './services/APIUtil'
 
-export default {
-  name: 'App',
-  components: {Login},
-  data () {
-    return {
-      lastCount: 0
-    }
-  },
-  computed: {
-    ...mapGetters([
-      'isLogin',
-      'role'
-    ])
-  },
-  methods: {
-    /**
-     * route to sub page
-     * @param name router's name
-     */
-    onSelected (name) {
-      this.$router.push({
-        name: name
-      })
-    },
-    checkUpdate () {
-      if (this.role === 'platformManager') {
-        APIUtil.get('/Commodity').then(response => {
-          if (response.status === 200) {
-            let list = response.data.Commodity
-            /*
-             * If count is not equal, get its last item
-             */
-            if (this.lastCount !== 0 && list.length !== this.lastCount) {
-              this.sendNoticeCommodity(list[list.length - 1])
-            }
-            /*
-             * Update lastCount
-             */
-            this.lastCount = list.length
-          }
-        })
+  export default {
+    name: 'App',
+    components: {Login},
+    data () {
+      return {
+        lastCount: 0
       }
     },
-    sendNoticeCommodity (newCommodity) {
-      let id = newCommodity.id
-      let commodityname = newCommodity.commodityname
-      //let examinationTime = newCommodity.timestamp * 1000
-      this.$Notice.info({
-        title: '商品 - ' + id,
-        name: id,
-        duration: 0,
-        render: createElement => {
-          return createElement('div', {
-              style: {
-                lineHeight: 1.5
-              },
-              on: {
-                click: () => {
-                  this.$router.push({
-                    name: 'CommodityDetail',
-                    params: {
-                      id: id
-                    }
-                  })
-                  this.$Notice.close(id)
-                }
-              }
-            },
-            [
-              createElement('p', ['商品名称: ' + commodityname]),
-              //createElement('p', ['开单时间: ' + Util.timeStampFormatter(examinationTime)])
-            ])
-        }
-      })
+    computed: {
+      ...mapGetters([
+        'isLogin',
+        'role'
+      ])
     },
-  },
-  mounted () {
-    setInterval(() => this.checkUpdate(), 2000)
+    methods: {
+      /**
+       * route to sub page
+       * @param name router's name
+       */
+      onSelected (name) {
+        this.$router.push({
+          name: name
+        })
+      },
+      checkUpdate () {
+        if (this.role === 'platformManager') {
+          APIUtil.get('/Commodity').then(response => {
+            if (response.status === 200) {
+              let list = response.data.Commodity
+              /*
+               * If count is not equal, get its last item
+               */
+              if (this.lastCount !== 0 && list.length !== this.lastCount) {
+                this.sendNoticeCommodity(list[list.length - 1])
+              }
+              /*
+               * Update lastCount
+               */
+              this.lastCount = list.length
+            }
+          })
+        }
+      },
+      sendNoticeCommodity (newCommodity) {
+        let id = newCommodity.id
+        let commodityname = newCommodity.commodityname
+        //let examinationTime = newCommodity.timestamp * 1000
+        this.$Notice.info({
+          title: '商品 - ' + id,
+          name: id,
+          duration: 0,
+          render: createElement => {
+            return createElement('div', {
+                style: {
+                  lineHeight: 1.5
+                },
+                on: {
+                  click: () => {
+                    this.$router.push({
+                      name: 'CommodityDetail',
+                      params: {
+                        id: id
+                      }
+                    })
+                    this.$Notice.close(id)
+                  }
+                }
+              },
+              [
+                createElement('p', ['商品名称: ' + commodityname]),
+                //createElement('p', ['开单时间: ' + Util.timeStampFormatter(examinationTime)])
+              ])
+          }
+        })
+      },
+    },
+    mounted () {
+      setInterval(() => this.checkUpdate(), 2000)
+    }
   }
-}
 </script>
 
 <style lang="scss" scoped>
@@ -141,6 +141,7 @@ export default {
     text-align: center;
     color: #2c3e50;
   }
+
   .layout-title {
     float: left;
     left: 20px;
@@ -148,6 +149,7 @@ export default {
     font-size: 16px;
     color: white;
   }
+
   .layout {
     border: 1px solid #d7dde4;
     background: #f5f7f9;
@@ -155,6 +157,7 @@ export default {
     border-radius: 4px;
     overflow: hidden;
   }
+
   .layout-logo {
     width: 100px;
     height: 30px;
@@ -164,7 +167,8 @@ export default {
     position: relative;
     top: 15px;
   }
-  .menu-item  {
+
+  .menu-item {
     span {
       display: inline-block;
       overflow: hidden;
@@ -188,6 +192,7 @@ export default {
       width: 0;
       transition: width .2s ease;
     }
+
     i {
       transform: translateX(5px);
       transition: font-size .2s ease .2s, transform .2s ease .2s;
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,49 +1,43 @@
 <template>
-    <div>
-      <Card :bordered="false" class="card">
-        <p slot="title">后台系统登录</p>
-         <Input v-model="name" prefix="ios-contact" placeholder="Username" style="width: 300px" />
-         <Input v-model="password"  type="password" prefix="ios-lock" placeholder="Password" style="width: 300px;margin-top: 10px" />
-        <p></p>
-         <Button type="primary" style="width: 100px; margin-top: 40px" @click="login">登录</Button>
-      </Card>
-    </div>
+  <div>
+    <Card style="width: 60%;margin: 15% auto;" :bordered="false" class="card">
+      <div style="font-weight: bold;" slot="title">后台系统登录</div>
+      <div>
+        <Input v-model="name" prefix="ios-contact" placeholder="Username"/>
+        <Input v-model="password" type="password" prefix="ios-lock" placeholder="Password"
+               style="margin-top: 10px"/>
+      </div>
+      <Button type="primary" style="width: 100px; margin-top: 40px" @click="login">登录</Button>
+    </Card>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'Login',
-  data () {
-    return {
-      name: '',
-      password: ''
-    }
-  },
-  methods: {
-    login () {
-      if (this.name === 'platform' && this.password === '123456') {
-        // 平台管理者
-        this.$store.dispatch('changeLoginState', true)
-        this.$store.dispatch('changeRole', 'platformManager')
-        this.$router.push({
-          name: 'PlatformManager'
-        })
-      } else if (this.name === 'merchant' && this.password === '123456') {
-        this.$store.dispatch('changeLoginState', true)
-        this.$store.dispatch('changeRole', 'merchant')
-        this.$router.push({
-          name: 'Merchant'
-        })
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        name: 'merchant',
+        password: '123456',
+      }
+    },
+    methods: {
+      login () {
+        if (this.name === 'platform' && this.password === '123456') {
+          // 平台管理者
+          this.$store.dispatch('changeLoginState', true)
+          this.$store.dispatch('changeRole', 'platformManager')
+          this.$router.push({
+            name: 'PlatformManager'
+          })
+        } else if (this.name === 'merchant' && this.password === '123456') {
+          this.$store.dispatch('changeLoginState', true)
+          this.$store.dispatch('changeRole', 'merchant')
+          this.$router.push({
+            name: 'Merchant'
+          })
+        }
       }
     }
   }
-}
 </script>
-
-<style lang="scss" scoped>
-  .card {
-    width: 500px;
-    margin-left: 450px;
-    margin-top: 50px;
-  }
-</style>
